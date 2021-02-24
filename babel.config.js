edd17ad10dd8e4bf9a664bb23614e52b2a16e4a8
Jesus Romero <jesus.rg@gmail.com>
2021-02-24 14:29:33 -0600

    install dependencies and config env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '~': './src',
+        },
+        cwd: 'babelrc',
+      },
+    ],
+    [
+      'dotenv-import',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
---
+++ src/core/env.js
@@ -0,0 +1,3 @@
+import {BASE_URL as BASE_URL_ENV} from '@env';
+
+export const BASE_URL = BASE_URL_ENV;
