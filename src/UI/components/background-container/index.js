525ae8e6f5c9e8643c147ff32484924795eca676
Jesus Romero <jesus.rg@gmail.com>
2021-02-25 21:10:45 -0600

    improve status bar config

--- src/UI/components/background-container/index.js
+++ src/UI/components/background-container/index.js
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import {View, Image, StatusBar} from 'react-native';
+import {View, Image} from 'react-native';
 import {styles} from './styles';
 import {backgroundImage} from '~/assets/images';
 
 export const BackgroundContainer = ({children}) => {
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
       <View style={styles.childContainer}>{children}</View>
       <Image style={styles.background} source={backgroundImage} />
     </View>
--- src/providers/app.provider/index.js
+++ src/providers/app.provider/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View} from 'react-native';
+import {View, StatusBar} from 'react-native';
 import {Loading} from '~/UI/components';
 import {styles} from './styles';
 import {useSelector} from 'react-redux';
@@ -8,6 +8,7 @@ export const AppProvider = ({children}) => {
   const loading = useSelector(({app}) => app.loading);
   return (
     <>
+      <StatusBar barStyle="light-content" />
       {loading && <Loading />}
       <View style={styles.childrenContainer}>{children}</View>
     </>
